bb8ca76af07644b44df823af14847100237f170a
Bichitra Kumar Sahoo <32828151+bichitra95@users.noreply.github.com>
2024-07-09 17:14:50 +0530

    Remove soda cloud

--- datacontracts/contract_verification.py
+++ datacontracts/contract_verification.py
@@ -4,7 +4,6 @@ contract_verification_result: ContractVerificationResult = (
      ContractVerification.builder()
      .with_contract_yaml_file('marketing_campaign.yaml')
      .with_data_source_yaml_file('/Users/bichitra.sahoo/atlan/projects/fe-contracts-lineage-workshop/.atlan/data_sources.yaml')
-     .with_soda_cloud_yaml_file("/Users/bichitra.sahoo/atlan/projects/fe-contracts-lineage-workshop/.atlan/soda_cloud.yaml")
      .execute()
 )
 
